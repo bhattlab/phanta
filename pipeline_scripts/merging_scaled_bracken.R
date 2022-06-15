b0c1db94fdfb1d52d8dcc5b76b6b96b4b0ea0c21
Meena Chakraborty <mchakra@smsh11dsu-srcf-d15-36.scg.stanford.edu>
2022-06-14 21:20:18 -0700

    update format of merged comm abund table

--- pipeline_scripts/merging_scaled_bracken.R
+++ pipeline_scripts/merging_scaled_bracken.R
@@ -11,27 +11,25 @@ scaled_reports <- as.character(read.csv(scaled_reports_list, header=FALSE)[,1])
 # find locations of '.' in the file names
 locations <- str_locate(scaled_reports, '\\.')[,1]
 sampnames <- substr(scaled_reports, 1, locations-1)
-desired_colnames <- c("TaxName", "TaxID", sampnames)
+desired_colnames <- c("TaxID", sampnames)
 
 # initialize data frame
 merged_table <- read.csv(paste0(outdir, '/', scaled_reports[1]), sep='\t', header=TRUE)
-name_col <- which(colnames(merged_table) == 'name')
 taxid_col <- which(colnames(merged_table) == 'taxonomy_id')
 community_col <- which(colnames(merged_table) == 'community_abundance')
-merged_table <- merged_table[,c(name_col, taxid_col, community_col)]
-colnames(merged_table) <- desired_colnames[1:3]
+merged_table <- merged_table[,c(taxid_col, community_col)]
+colnames(merged_table) <- desired_colnames[1:2]
 
 # loop through files and keep merging
 if (length(scaled_reports) > 1) {
 for (i in seq(2,length(scaled_reports))) {
   f <- paste0(outdir, '/', scaled_reports[i])
   table_to_merge <- read.csv(f, sep='\t', header=TRUE)
-  name_col <- which(colnames(table_to_merge) == 'name')
   taxid_col <- which(colnames(table_to_merge) == 'taxonomy_id')
   community_col <- which(colnames(table_to_merge) == 'community_abundance')
-  table_to_merge <- table_to_merge[,c(name_col, taxid_col, community_col)]
-  colnames(table_to_merge) <- c("TaxName", "TaxID", desired_colnames[i+2])
-  merged_table <- merge(merged_table, table_to_merge, by = c("TaxName", "TaxID"), all=TRUE)
+  table_to_merge <- table_to_merge[,c(taxid_col, community_col)]
+  colnames(table_to_merge) <- c("TaxID", desired_colnames[i+2])
+  merged_table <- merge(merged_table, table_to_merge, by = c("TaxID"), all=TRUE)
 }}
 
 # replace NA with 0
--- pipeline_scripts/merging_scaled_bracken.py
+++ pipeline_scripts/merging_scaled_bracken.py
@@ -2,6 +2,8 @@ import sys
 
 db, temp_file, outdir = sys.argv[1], sys.argv[2], sys.argv[3]
 
+# make a few helpful dictionaries
+
 with open(db + '/taxonomy/nodes.dmp', 'r') as infile:
 
   # make a child to parent dictionary
@@ -14,33 +16,51 @@ with open(db + '/taxonomy/nodes.dmp', 'r') as infile:
     child_to_parent[child] = parent
     taxid_to_rank[child] = rank
 
-def taxid_to_lineage(taxid, taxon_name):
+# also want a taxid_to_name dictionary
+# use the inspect.out file
+
+with open(db + '/inspect.out', 'r') as infile:
+  taxid_to_name = {}
+  for line in infile:
+    line=line.rstrip('\n').split('\t')
+    taxid, name = line[4], line[5].strip()
+    taxid_to_name[taxid] = name
+
+def taxid_to_lineages(taxid):
   # look up the specific taxid,
-  # build the lineage using the dictionaries
+  # build the lineages using the dictionaries
 
   # first deal with the special case of unclassiifed
   if taxid == '0':
     return 'unclassified'
 
   # not unclassified - proceed
-  lineage = taxid_to_rank[taxid] + '_' + taxon_name
+  rank = taxid_to_rank[taxid]
+  lineage_taxids = rank + '_' + taxid
+  lineage_names = rank + '_' + taxid_to_name[taxid]
   child, parent = taxid, None
 
   while not parent == '1':
-    # look up child, add to lineage
+    # look up child, add to lineages
     parent = child_to_parent[child]
-    rank = taxid_to_rank[parent]
-    lineage = rank + '_' + parent + '|' + lineage
+    parent_rank = taxid_to_rank[parent]
+    # look up the name of the parent too
+    parent_name = taxid_to_name[parent]
+    lineage_taxids = parent_rank + '_' + parent + '|' + lineage_taxids
+    lineage_names = parent_rank + '_' + parent_name + '|' + lineage_names
     child = parent # needed for recursion
-  return lineage
+  return lineage_names, lineage_taxids
 
 with open(temp_file, 'r') as infile:
   with open(outdir + '/corrected_relative_abundance.txt', 'w') as outfile:
-    outfile.write(infile.readline())
+    # first fix the header
+    orig_header = infile.readline().rstrip('\n').split('\t')
+    new_header = '\t'.join(['Taxon_Lineage_with_Names', 'Taxon_Lineage_with_IDs'] + orig_header[1:]) + '\n'
+    outfile.write(new_header)
     for line in infile:
       line=line.rstrip('\n').split('\t')
       # use the function to figure out what we want to call this line
-      taxname, taxid = line[0], line[1]
-      proper_name = taxid_to_lineage(taxid, taxname)
-      outlist = [proper_name] + line[1:]
+      taxid = line[0]
+      lin_names, lin_ids = taxid_to_lineages(taxid)
+      outlist = [lin_names, lin_ids] + line[1:]
       outfile.write('\t'.join(outlist) + '\n')
