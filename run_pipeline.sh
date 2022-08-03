4a90d9100f2e7c6e1c687ef72a6cf3dbd5cfa9a0
Meena Chakraborty <mchakra@smsh11dsu-srcf-d15-38.scg.stanford.edu>
2022-08-03 16:27:20 -0700

    remove unofficial file

--- run_pipeline.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-snakemake -s /labs/asbhatt/mchakra/phaging/phage_abundance_pipeline/Snakefile \
---configfile /labs/asbhatt/mchakra/phaging/phage_abundance_pipeline/testing/config_test.yaml \
---jobs 999 --reason --profile scg
